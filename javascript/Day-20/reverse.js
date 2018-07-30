48f151f1d31f5a9e3277b8c5b8f2f83b908a3e27
Deepanshu Garg <deepanshu.garg24jan@gmail.com>
2018-07-30 23:38:21 +0530

    code update

---
+++ javascript/Day-20/reverse.js
@@ -0,0 +1,18 @@
+var num,a;
+var arr=[];
+
+
+var readline=require('readline');
+var prompts=readline.createInterface(process.stdin,process.stdout);
+prompts.question('Enter any number',function(num){
+   var i=0;
+
+    while(num!=0)
+     {
+         a=parseInt(num%2);
+         arr[i]=a;
+         num=parseInt(num/2);
+         i++;
+     }
+     console.log(arr);
+    });
