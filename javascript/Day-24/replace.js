08bd97b77645e81ea63aa7a6266849dfbb79c542
Deepanshu Garg <deepanshu.garg24jan@gmail.com>
2018-08-04 00:03:09 +0530

    code updated

---
+++ javascript/Day-24/replace.js
@@ -0,0 +1,17 @@
+<html>
+   <head>
+      <title>JavaScript String replace() Method</title>
+   </head>
+   
+   <body>
+   
+      <script type="text/javascript">
+         var re = /apples/gi;
+         var str = "Kiet group of institutions.";
+         var newstr = str.replace(re, "group");
+         
+         document.write(newstr ); 
+      </script>
+      
+   </body>
+</html>
